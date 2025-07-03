78a3515f9950ad1647bc1a28a4b55874f105dcf6
M-Shourav <muhammadshourav625@gmail.com>
2025-07-03 10:25:50 +0600

    userlist button

--- components/userList/UserList.tsx
+++ components/userList/UserList.tsx
@@ -114,45 +114,36 @@ const UserList = () => {
                     {item?.isAdmin ? "Admin" : "User"}
                   </TableCell>
                   <TableCell className="hidden md:flex items-center justify-center">
-                    {item?.isAdmin ? (
-                      <Button
-                        variant="destructive"
-                        className="cursor-not-allowed bg-transparent text-black hover:text-black"
-                      >
-                        <Trash2 size={18} />
-                      </Button>
-                    ) : (
-                      <AlertDialog>
-                        <AlertDialogTrigger asChild>
-                          <Button
-                            className="cursor-pointer bg-transparent text-black hover:text-white"
-                            variant={"destructive"}
+                    <AlertDialog>
+                      <AlertDialogTrigger asChild>
+                        <Button
+                          className="cursor-pointer bg-transparent text-black hover:text-white"
+                          variant={"destructive"}
+                        >
+                          <Trash2 size={18} />
+                        </Button>
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>Delete User</AlertDialogTitle>
+                          <AlertDialogDescription>
+                            This action will permanently remove the user from
+                            the system. Are you sure?
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel className="cursor-pointer">
+                            Cancel
+                          </AlertDialogCancel>
+                          <AlertDialogAction
+                            onClick={() => handleRemoveUser(item?._id)}
+                            className=" cursor-pointer"
                           >
-                            <Trash2 size={18} />
-                          </Button>
-                        </AlertDialogTrigger>
-                        <AlertDialogContent>
-                          <AlertDialogHeader>
-                            <AlertDialogTitle>Delete User</AlertDialogTitle>
-                            <AlertDialogDescription>
-                              This action will permanently remove the user from
-                              the system. Are you sure?
-                            </AlertDialogDescription>
-                          </AlertDialogHeader>
-                          <AlertDialogFooter>
-                            <AlertDialogCancel className="cursor-pointer">
-                              Cancel
-                            </AlertDialogCancel>
-                            <AlertDialogAction
-                              onClick={() => handleRemoveUser(item?._id)}
-                              className=" cursor-pointer"
-                            >
-                              Continue
-                            </AlertDialogAction>
-                          </AlertDialogFooter>
-                        </AlertDialogContent>
-                      </AlertDialog>
-                    )}
+                            Continue
+                          </AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
                   </TableCell>
                   <TableCell className="text-center">
                     <UpdateUser user={item} onUpdate={getUserList} />
